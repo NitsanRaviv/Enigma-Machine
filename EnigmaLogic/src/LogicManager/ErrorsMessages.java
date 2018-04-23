670046efc15eb2f4eab1b413475dedfb235a665e
nr1617@gmail.com <bhmi858285>
2018-04-23 16:23:59 +0300

    fix infinity loop

--- EnigmaLogic/src/LogicManager/ErrorsMessages.java
+++ EnigmaLogic/src/LogicManager/ErrorsMessages.java
@@ -3,15 +3,15 @@ package LogicManager;
 public interface ErrorsMessages {
 
     public String noErrors = "";
-    public String errNoMachine = "tried get info about the machine without load one";
-    public String errXML = "the file is not xml type";
-    public String errGetMachine = "error reading machine";
+    public String errNoMachine = "Tried get info about the machine without load one";
+    public String errXML = "This file is not xml type or invalid";
+    public String errGetMachine = "Error reading machine";
     public String errABCSize = "ABC size is not even";
-    public String errAmountOfRotor = "not enough rotors";
-    public String errLessThenTwoRotor = "less then two rotors";
-    public String errIDsRotors = "error in rotors ids";
-    public String errDuplicateMappings = "error duplicate mappings";
-    public String errNotch = "error notch";
-    public String errIDsReflector = "error in reflector ids";
-    public String errReflect = "reflect error";
+    public String errAmountOfRotor = "There is not enough rotors in the file";
+    public String errLessThenTwoRotor = "There is less then two rotors in the file";
+    public String errIDsRotors = "Error in rotors ids";
+    public String errDuplicateMappings = "Error duplicate mappings";
+    public String errNotch = "Error notch";
+    public String errIDsReflector = "Error in reflector ids";
+    public String errReflect = "Reflect error";
 }
