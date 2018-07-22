0cf507c8d8222a501f2aa8fd825fdd04f982be24
nr1617@gmail.com <bhmi858285>
2018-07-22 17:37:49 +0300

    add the missing JS file

---
+++ EnigmaServer/web/Static Pages/assets/js/selectCompetition.js
@@ -0,0 +1,31 @@
+var availableGames_url = buildUrlWithContextPath("competitionStart");
+
+$(function () {
+
+    //prevent IE from caching ajax calls
+    $.ajaxSetup({cache: false});
+
+    getAvailableGames();
+    
+
+});
+
+  function getAvailableGames() {
+    $.ajax({
+        url: availableGames_url,
+        datatype: 'json',
+        success: function (data) {
+            if (data) {
+                availableGames = $.parseJSON(data);
+                var i = 1;
+                for (var url in availableGames) {
+                    var game = document.createElement("P");
+                    var t_game = document.createTextNode( i + ". " + availableGames[url]);  
+                    game.appendChild(t_game);
+
+                    document.getElementById("availableCompetition").appendChild(game);
+                    i++;
+                }
+            }
+        }
+    });
