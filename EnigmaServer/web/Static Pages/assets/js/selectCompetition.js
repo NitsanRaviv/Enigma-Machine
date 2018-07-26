74c8e7f26d75f334c0f2739da26bd514a9f8e498
nr1617@gmail.com <bhmi858285>
2018-07-26 13:10:23 +0300

    fgr

--- EnigmaServer/web/Static Pages/assets/js/selectCompetition.js
+++ EnigmaServer/web/Static Pages/assets/js/selectCompetition.js
@@ -1,33 +1,32 @@
-//var availableGames_url = buildUrlWithContextPath("openCompetitions");
 var availableGames_url = "localhost:8080/openCompetitions";
 
-$(function () {
+      $(function () {
 
-    //prevent IE from caching ajax calls
-    $.ajaxSetup({cache: false});
+          //prevent IE from caching ajax calls
+          $.ajaxSetup({cache: false});
 
-    getAvailableGames();
-    
+          getAvailableGames();
 
-}
 
-  function getAvailableGames() {
-      $.ajax({
-          url: availableGames_url,
-          datatype: 'json',
-          success: function (data) {
-              if (data) {
-                  availableGames = $.parseJSON(data);
-                  var i = 1;
-                  for (var url in availableGames) {
-                      var game = document.createElement("P");
-                      var t_game = document.createTextNode(i + ". " + availableGames[url]);
-                      game.appendChild(t_game);
+      });
 
-                      document.getElementById("availableCompetition").appendChild(game);
-                      i++;
+      function getAvailableGames() {
+          $.ajax({
+              url: availableGames_url,
+              datatype: 'json',
+              success: function (data) {
+                  if (data) {
+                      availableGames = $.parseJSON(data);
+                      var i = 1;
+                      for (var url in availableGames) {
+                          var game = document.createElement("P");
+                          var t_game = document.createTextNode(i + ". " + availableGames[url]);
+                          game.appendChild(t_game);
+
+                          document.getElementById("availableCompetition").appendChild(game);
+                          i++;
+                      }
                   }
               }
-          }
-      });
-  }
+          });
+      }
